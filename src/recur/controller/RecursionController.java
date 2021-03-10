7bc95cbd2df119147882b3b51fb63e29ab39c977
DylanLindquist <70230076+DylanLindquist@users.noreply.github.com>
2021-03-10 13:53:39 -0700

    stuff

--- src/recur/controller/RecursionController.java
+++ src/recur/controller/RecursionController.java
@@ -1,12 +1,63 @@
 package recur.controller;
 
+import recur.view.RecursionPopup;
+
 public class RecursionController
 {
-
+	private RecursionPopup view;
+	
+	public RecursionController()
+	{
+		this.view = new RecursionPopup();
+	}
+	
 	public void start()
 	{
+		String text = view.askQuestion("What should we try or blank to exit");
 		
+		while (!text.contentEquals(""))
+		{
+			String words = mystery(text);
+			view.displayMessage(words);
+			
+			text = view.askQuestion("Type in the next value or blank to exit.");
+		}
+	}
+	
+	private int fibRecursive(int input)
+	{
+		if(input == 0 || input == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return fibRecursive(input - 1) + fibRecursive(input -2);
+		}
+	}
+	
+	private double factorial(double number)
+	{
+		if(number == 0 || number == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return factorial(number * factorial(number -1));
+		}
 		
+	}
+	private String mystery(String text)
+	{
+		if (text.length() ==1)
+		{
+			return text;
+		}
+		else
+		{
+			return mystery (text.substring(1)) + text.substring(0,1);
+		}
 	}
 
 }
--- src/recur/view/RecursionPopup.java
+++ src/recur/view/RecursionPopup.java
@@ -1,6 +1,12 @@
 package recur.view;
 
+import javax.swing.JOptionPane;
+
 public class RecursionPopup
 {
-
+	 public void showMessage(String message)
+	 {
+		 JOptionPane.showMessageDialog(null, message);
+		 
+	 }
 }
